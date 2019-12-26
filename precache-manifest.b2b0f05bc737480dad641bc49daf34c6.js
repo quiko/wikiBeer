f619757f29eff15ddb9b81bd77c2e4cf78e1b104
quiko <ouladch@gmail.com>
2019-12-26 16:32:31 +0100

    Updates

---
+++ precache-manifest.b2b0f05bc737480dad641bc49daf34c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "150e6b63059d8929e2cba0ed61d62a00",
+    "url": "/wikiBeer/index.html"
+  },
+  {
+    "revision": "a4a7f6e7c6c341261ede",
+    "url": "/wikiBeer/static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "d100e4b2324d85ce73b5",
+    "url": "/wikiBeer/static/js/2.75d34b70.chunk.js"
+  },
+  {
+    "revision": "37ef80829e57f4a0c2aa4bfe70fe2cf3",
+    "url": "/wikiBeer/static/js/2.75d34b70.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a4a7f6e7c6c341261ede",
+    "url": "/wikiBeer/static/js/main.2f167b53.chunk.js"
+  },
+  {
+    "revision": "49ceab0bd7093da303fc",
+    "url": "/wikiBeer/static/js/runtime-main.7ad037a8.js"
+  },
+  {
+    "revision": "6518cad58c8d235b1eb533fe7ee75752",
+    "url": "/wikiBeer/static/media/beer.6518cad5.jpg"
+  }
+]);
